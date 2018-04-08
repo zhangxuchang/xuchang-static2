31167edbc505e6e7d425b2dbdbebf5d962e08607
zxc <44724385@qq.com>
2018-04-08 16:14:25 +0800

    sso guard implement

--- plugins/core-sso-guard/sso-guard.js
+++ plugins/core-sso-guard/sso-guard.js
@@ -6,12 +6,47 @@
 
 window.OasisCoreSSOGuard = {
 
-    coreProxyPage: "http://sso-guard.oasisgames.cn/research/post-message/child.html?1",
+    coreProxyPage: "http://sso-guard.oasisgames.cn/research/post-message/child.html?2",
+    loginCookies: [],
+    loginSessionStartTime: 0,
+    loginUrl: '',
+    guardStarted: true,
 
     initialize: function (data) {
-        console.log(data);
-        OasisCoreSSOGuard.installProxy();
-        OasisCoreSSOGuard.eventRegister();
+        this.dataInitialize(data);
+        this.installProxy();
+        this.eventRegister();
+    },
+    
+    dataInitialize: function (data) {
+
+        if(typeof(data.loginCookies) == 'undefined' || data.loginCookies.length == 0) {
+            this.output('parameter missing: data-login-cookies');
+            this.guardStarted = false;
+        }
+        else
+        {
+            this.loginCookies = data.loginCookies.split(",");
+        }
+        if(typeof(data.loginSessionStartTime) == 'undefined' || data.loginSessionStartTime.length == 0) {
+            this.output('parameter missing: data-login-session-start-time');
+            this.guardStarted = false;
+        }
+        else
+        {
+            this.loginSessionStartTime = parseInt(data.loginSessionStartTime);
+        }
+        if(typeof(data.loginUrl) == 'undefined' || data.loginUrl.length == 0) {
+            this.output('parameter missing: data-login-url');
+            this.guardStarted = false;
+        }
+        else{
+            this.loginUrl = data.loginUrl;
+        }
+    },
+
+    output: function (msg) {
+        console.log("[sso-guard] " + msg);
     },
 
     deleteCookie: function (name) {
@@ -19,23 +54,43 @@ window.OasisCoreSSOGuard = {
     },
     
     installProxy: function () {
-        var html = '<iframe id="oasis-core-sso-gruard-frame" src="'+ OasisCoreSSOGuard.coreProxyPage+'" style="display: none"></iframe>';
+        var html = '<iframe id="oasis-core-sso-gruard-frame" src="'+ this.coreProxyPage+'" style="display: none"></iframe>';
         var body = $('body');
         body.append(html);
-        console.log('install proxy page');
-    },
-    
-    eventRegister: function () {
-        var handleReceive = function (data) {
-            OasisCoreSSOGuard.ssoGuard(data);
-        };
-        window.addEventListener("message",handleReceive,false);
+        this.output('install proxy page');
     },
 
     ssoGuard: function (data) {
-        console.log('sso-guard');
-        console.log(data.data);
+
+        if(this.guardStarted == false){
+            this.output('not started');
+            return;
+        }
+
+        console.log(data);
+
+        if(typeof(data.user_login_time) == 'undefined'){
+            this.output("can not read 'user_login_time' from proxy message");
+            return;
+        }
+        if(data.user_login_time <= this.loginSessionStartTime){
+            return;
+        }
+        // clear login cookie and redirect to login url
+        for(var i in this.loginCookies){
+            this.deleteCookie(this.loginCookies[i]);
+            this.output('delete cookie: ' + this.loginCookies[i]);
+        }
+
+        //window.top.location.href = this.loginUrl;
     },
+
+    eventRegister: function () {
+        var handleReceive = function (data) {
+            OasisCoreSSOGuard.ssoGuard(data.data);
+        };
+        window.addEventListener("message",handleReceive,false);
+    }
 };
 
 $(function () {
