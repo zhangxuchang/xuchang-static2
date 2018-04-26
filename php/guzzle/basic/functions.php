bb5c6f1ada679ebeab228edd0b70ee0c09340f80
zxc <44724385@qq.com>
2018-04-26 16:31:58 +0800

    *

--- php/guzzle/basic/functions.php
+++ php/guzzle/basic/functions.php
@@ -7,17 +7,23 @@
  */
 
 /**
- * @param $msg
+ * @param     $msg
+ * @param int $lineHeight
  */
-function myecho($msg)
+function myecho($msg, $lineHeight = 0)
 {
 
     if (is_array($msg)) {
         print_r($msg);
+        echo PHP_EOL;
     }
     else {
         echo $msg;
-        echo "\n\n";
+        echo "\n";
+    }
+
+    for ($i = 0; $i < $lineHeight; $i++) {
+        echo PHP_EOL;
     }
 }
 
@@ -27,9 +33,9 @@ function envDes()
 {
     if (USE_PROXY == true) {
         myecho("start test with proxy switch is on");
-        myecho("proxy uri: " . OAS_PROXY);
+        myecho("proxy uri: " . OAS_PROXY, 1);
     }
     else {
-        myecho("start test with proxy switch is off");
+        myecho("start test with proxy switch is off", 1);
     }
 }
