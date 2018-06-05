2d2a06a96db6a25983600cef963767f2bda68d2e
zxc <44724385@qq.com>
2018-06-05 16:24:21 +0800

    *

---
+++ slimvue-test/test/entries/level2/subpage.js
@@ -0,0 +1,4 @@
+import slimvue from 'slimvue';
+import App     from 'test/components/SubApp.vue';
+
+slimvue.mount(App);
---
+++ slimvue-test/test/publish.js
@@ -0,0 +1,7 @@
+import MyClock   from 'test/components/Clock';
+
+export { MyClock };
+
+export default {
+    MyClock
+};
