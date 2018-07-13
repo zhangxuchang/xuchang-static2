8aebd138273c69b5e18bd9475f6bbdac3110c943
zxc <44724385@qq.com>
2018-07-13 11:28:10 +0800

    test code

--- php/test.php
+++ php/test.php
@@ -1,15 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xuchang
- * Date: 18/5/2
- * Time: 13:07
- */
-
-$url = "http://www.facebook.com/dialog/oauth?client_id=562265367280905&redirect_uri="
-       . urlencode("https://apps.facebook.com/naruto_fr")
-       . "&scope=email,user_friends";
 
 
-echo $url;
-echo PHP_EOL;
+echo 'sleep 10';
+
+sleep(10);
