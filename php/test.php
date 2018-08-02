fdc525e0311f2d5892c4c83ad7c8cdf9aa7a40ff
zxc <44724385@qq.com>
2018-08-02 19:39:46 +0800

    *

--- php/test.php
+++ php/test.php
@@ -1,6 +1,23 @@
 <?php
 
+function myPrint($data)
+{
+    print_r($data);
 
-echo 'sleep 10';
+    echo PHP_EOL;
+}
 
-sleep(10);
+$TAG_PATTERN = "/^(v|V)?(?P<major>[0-9]+)(\\.(?P<minor>[0-9]+))?(\\.(?P<patch>[0-9]+))?(-alpha|-beta)?[0-9]+\$/";
+
+$tag  = 'v1.0.0';
+$tag2 = 'v1.0.0-beta2';
+
+$matchs = [];
+
+myPrint(preg_match($TAG_PATTERN, $tag, $matchs));
+
+myPrint($matchs);
+
+myPrint(preg_match($TAG_PATTERN, $tag2, $matchs));
+
+myPrint($matchs);
