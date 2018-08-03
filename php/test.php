14663e75ba7e2a909a69425b2471cc42a86ae976
zxc <44724385@qq.com>
2018-08-03 10:12:48 +0800

    *

--- php/test.php
+++ php/test.php
@@ -7,9 +7,9 @@ function myPrint($data)
     echo PHP_EOL;
 }
 
-$TAG_PATTERN = "/^(v|V)?(?P<major>[0-9]+)(\\.(?P<minor>[0-9]+))?(\\.(?P<patch>[0-9]+))?(-alpha|-beta)?[0-9]+\$/";
+$TAG_PATTERN = "/^(v|V)?(?P<major>[0-9]+)(\\.(?P<minor>[0-9]+))?(\\.(?P<patch>[0-9]+))?(-alpha|-beta)[0-9]+\$/";
 
-$tag  = 'v1.0.0';
+$tag  = 'v12.12.222-alpha1';
 $tag2 = 'v1.0.0-beta2';
 
 $matchs = [];
@@ -18,6 +18,6 @@ myPrint(preg_match($TAG_PATTERN, $tag, $matchs));
 
 myPrint($matchs);
 
-myPrint(preg_match($TAG_PATTERN, $tag2, $matchs));
-
-myPrint($matchs);
+//myPrint(preg_match($TAG_PATTERN, $tag2, $matchs));
+//
+//myPrint($matchs);
